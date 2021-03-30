8891a267e0420fe7980b3b0b65753f9b34cae6da
BurningKarl <10158964+BurningKarl@users.noreply.github.com>
2021-03-30 13:30:30 +0200

    Added weights and biases logging

--- sketching/config.py
+++ sketching/config.py
@@ -5,17 +5,25 @@ import json
 from typing import Optional
 
 
+class Preconditioning(enum.Enum):
+    NONE = 'none'
+    SKETCHING = 'sketching'
+    FULL = 'full'
+
+
 class LinearSolver(enum.Enum):
     CG_METHOD = 'cg_method'
     CHOLESKY = 'cholesky'
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class SketchingConfig:
+    number_of_runs: int = 10
     m: Optional[int] = 100
     n: Optional[int] = 10000
     nnz_factor: Optional[float] = 2
     density: float = dataclasses.field(init=False)
+    preconditioning: Preconditioning = Preconditioning.SKETCHING
     w_factor: Optional[float] = 1.5
     w: int = dataclasses.field(init=False)
     s: Optional[int] = 3
@@ -32,6 +40,7 @@ class SketchingConfig:
     def __post_init__(self):
         """ Validates the current configuration """
         try:
+            assert self.number_of_runs >= 1
             assert 1 <= self.m <= self.n
             assert 0 <= self.nnz_factor <= self.m
             assert self.w_factor >= 1
@@ -41,7 +50,7 @@ class SketchingConfig:
             raise ValueError("Invalid value in configuration") from error
 
         # nnz(A) = self.nnz_factor * self.n
-        self.density = self.nnz_factor / self.m
+        object.__setattr__(self, 'density', self.nnz_factor / self.m)
 
         # Set w according to the given w_factor
-        self.w = int(self.w_factor * self.m)
+        object.__setattr__(self, 'w', int(self.w_factor * self.m))
--- sketching/main.py
+++ sketching/main.py
@@ -6,11 +6,14 @@ __author__ = "Karl Welzel"
 __license__ = "GPLv3"
 
 import argparse
+import dataclasses
 import numpy as np
 import scipy
 import scipy.sparse
+import time
+import wandb
 from logzero import logger
-from config import SketchingConfig
+from config import SketchingConfig, Preconditioning
 from utils import random_sparse_coefficient_matrix, sparse_sketch
 
 CONFIG_FILE_PARAM = "config_file"
@@ -26,21 +29,40 @@ def main(args):
         config = SketchingConfig.from_file(config_file)
     logger.info(config)
 
-    coeff_matrix = random_sparse_coefficient_matrix(config.m, config.n, density=config.density)
-    diag_partition = np.random.choice([0, 1], size=config.n)
+    for i in range(config.number_of_runs):
+        run = wandb.init(project='sketching-ipm-condition-number', config=dataclasses.asdict(config), reinit=True)
 
-    for mu in np.logspace(0, -2, 30):
-        diag = scipy.sparse.diags(diag_partition * mu + (np.ones(config.n) - diag_partition) * (1 / mu))
-        spd_matrix = coeff_matrix * diag * coeff_matrix.T
-        logger.info(f'Unpreconditioned {mu:.1E} {np.linalg.cond(spd_matrix.toarray())}')
+        coeff_matrix = random_sparse_coefficient_matrix(config.m, config.n, density=config.density)
+        basis_probability = config.m / config.n  # At most m elements are in a basis
+        basis_partition = np.random.choice([1, 0], p=[basis_probability, 1 - basis_probability], size=config.n)
+        non_basis_partition = np.ones(config.n) - basis_partition
 
-        half_diag = scipy.sparse.diags(
-            diag_partition * np.sqrt(mu) + (np.ones(config.n) - diag_partition) * (1 / np.sqrt(mu)))
-        sketched_matrix = sparse_sketch(config.w, config.n, config.s) * half_diag * coeff_matrix.T
-        q, r = np.linalg.qr(sketched_matrix.toarray())
-        preconditioned_spd_matrix = np.linalg.inv(r.T) @ spd_matrix.toarray() @ np.linalg.inv(r)
-        logger.info(f'Preconditioned   {mu:.1E} {np.linalg.cond(preconditioned_spd_matrix)}')
-        # logger.info(np.linalg.cond(diag_matrix.toarray()))
+        for mu in np.logspace(-3, 0, 30):
+            sqrt_mu = np.sqrt(mu)
+            half_diag = scipy.sparse.diags(basis_partition * (1 / sqrt_mu) + non_basis_partition * sqrt_mu)
+            spd_matrix = coeff_matrix * half_diag * half_diag * coeff_matrix.T
+
+            start_time = time.time()
+            if config.preconditioning is Preconditioning.NONE:
+                condition_number = np.linalg.cond(spd_matrix.toarray())
+                wandb.log({'mu': mu, 'condition_number': condition_number, 'duration': time.time() - start_time})
+                logger.info(f'Unpreconditioned {mu:.1E} {condition_number}')
+            elif config.preconditioning is Preconditioning.SKETCHING:
+                sketched_matrix = sparse_sketch(config.w, config.n, config.s) * half_diag * coeff_matrix.T
+                # logger.info(f'Sketched dimensions: {sketched_matrix.shape}')
+                q, r = np.linalg.qr(sketched_matrix.toarray())
+                preconditioned_spd_matrix = np.linalg.inv(r.T) @ spd_matrix.toarray() @ np.linalg.inv(r)
+                condition_number = np.linalg.cond(preconditioned_spd_matrix)
+                wandb.log({'mu': mu, 'condition_number': condition_number, 'duration': time.time() - start_time})
+                logger.info(f'SkPreconditioned {mu:.1E} {condition_number}')
+            elif config.preconditioning is Preconditioning.FULL:
+                # logger.info(f'Original dimensions: {(half_diag * coeff_matrix.T).shape}')
+                q, r = np.linalg.qr((half_diag * coeff_matrix.T).toarray())
+                preconditioned_spd_matrix = np.linalg.inv(r.T) @ spd_matrix.toarray() @ np.linalg.inv(r)
+                condition_number = np.linalg.cond(preconditioned_spd_matrix)
+                wandb.log({'mu': mu, 'condition_number': condition_number, 'duration': time.time() - start_time})
+                logger.info(f'QRPreconditioned {mu:.1E} {condition_number}')
+        run.finish()
 
 
 if __name__ == "__main__":
