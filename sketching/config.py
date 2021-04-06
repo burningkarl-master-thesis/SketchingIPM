58110c12e04557ec9f065c4a3866e735d33531f6
BurningKarl <10158964+BurningKarl@users.noreply.github.com>
2021-04-06 14:08:42 +0200

    Refactored the configuration classes

--- sketching/config.py
+++ sketching/config.py
@@ -6,6 +6,9 @@ import json
 import typing
 
 
+T = typing.TypeVar("T")
+
+
 class Preconditioning(enum.Enum):
     NONE = "none"
     QR = "qr"
@@ -13,22 +16,9 @@ class Preconditioning(enum.Enum):
     CHOLESKY = "cholesky"
 
 
-@dataclasses.dataclass(frozen=True)
-class SketchingConfig:
-    """ Stores all configuration options for the sketching experiments """
-
-    number_of_runs: int = 1
-    m: int = 100
-    n: int = 10000
-    nnz_factor: float = 2
-    density: float = dataclasses.field(init=False)
-    preconditioning: Preconditioning = Preconditioning.QR
-    w_factor: float = 1.5
-    w: int = dataclasses.field(init=False)
-    s: int = 3
-
+class DaciteFromFile:
     @classmethod
-    def from_file(cls, filename: str) -> "SketchingConfig":
+    def from_file(cls: typing.Type[T], filename: str) -> T:
         """ Generates a SketchingConfig from a JSON configuration file """
         with open(filename) as f:
             json_config = json.load(f)
@@ -36,60 +26,118 @@ class SketchingConfig:
             cls, json_config, dacite.Config(strict=True, cast=[enum.Enum])
         )
 
+
+@dataclasses.dataclass(frozen=True)
+class ProblemConfig(DaciteFromFile):
+    """ Stores all configuration options for the problem instance """
+
+    m: int = 100
+    n: int = 10000
+    nnz_factor: float = 2
+    density: float = dataclasses.field(init=False)
+
     def __post_init__(self):
         """ Validates the current configuration """
         try:
-            assert self.number_of_runs >= 1
             assert 1 <= self.m <= self.n
             assert 0 <= self.nnz_factor <= self.m
-            assert self.w_factor >= 1
-            assert self.s >= 1
         except AssertionError as error:
             raise ValueError("Invalid value in configuration") from error
 
         # nnz(A) = self.nnz_factor * self.n
         object.__setattr__(self, "density", self.nnz_factor / self.m)
 
+
+@dataclasses.dataclass(frozen=True)
+class SketchingConfig(DaciteFromFile):
+    """ Stores all configuration options for the sketching experiments """
+
+    w_factor: float = 1.5
+    w: int = dataclasses.field(init=False)
+    s: int = 3
+
+    def __post_init__(self):
+        """ Validates the current configuration """
+        try:
+            assert self.w_factor >= 1
+            assert self.s >= 1
+        except AssertionError as error:
+            raise ValueError("Invalid value in configuration") from error
+
         # Set w according to the given w_factor
         object.__setattr__(self, "w", int(self.w_factor * self.m))
 
 
-# The following construction assumes that all fields of SketchingConfig have a default, not a default_factory
-list_config_fields = [
-    (
-        field.name,
-        typing.List[field.type],
-        dataclasses.field(
-            default_factory=lambda default=field.default: [default],
-            init=True,
-            repr=field.repr,
-            hash=field.hash,
-            compare=field.compare,
-            metadata=field.metadata,
-        ),
-    )
-    for field in dataclasses.fields(SketchingConfig)
-    if field.init
-]
-SketchingConfigProductSuper = dataclasses.make_dataclass(
-    "SketchingConfigProductSuper", list_config_fields, frozen=True
-)
+@dataclasses.dataclass(frozen=True)
+class PreconditioningConfig(DaciteFromFile):
+    preconditioning: Preconditioning = Preconditioning.QR
 
 
-class SketchingConfigProduct(SketchingConfigProductSuper):
-    """ Stores a list of possible parameter values for each option in SketchingConfig """
-
-    @classmethod
-    def from_file(cls, filename: str) -> "SketchingConfigProduct":
-        """ Generates a SketchingConfigProduct from a JSON configuration file """
-        with open(filename) as f:
-            json_config = json.load(f)
-        return dacite.from_dict(
-            cls, json_config, dacite.Config(strict=True, cast=[enum.Enum])
+def make_product_class(cls: typing.Type[T]):
+    """ Creates a new dataclass turning all fields of type T into type List[T] """
+    # The following construction assumes that all fields of cls have a default, not a default_factory
+    list_config_fields = [
+        (
+            field.name,
+            typing.List[field.type],
+            dataclasses.field(
+                default_factory=lambda default=field.default: [default],
+                init=True,
+                repr=field.repr,
+                hash=field.hash,
+                compare=field.compare,
+                metadata=field.metadata,
+            ),
         )
+        for field in dataclasses.fields(cls)
+        if field.init
+    ]
 
-    def configs(self):
-        """ Generates a SketchingConfig for each possible combination of the given parameter values """
+    def configs(self) -> T:
+        """ Generates a config object for each possible combination of the given parameter values """
         keys, values = zip(*dataclasses.asdict(self).items())
         for values_combination in itertools.product(*values):
-            yield SketchingConfig(**dict(zip(keys, values_combination)))
+            yield cls(**dict(zip(keys, values_combination)))
+
+    ConfigProduct = dataclasses.make_dataclass(
+        cls_name=cls.__name__ + "Product",
+        fields=list_config_fields,
+        bases=(DaciteFromFile,),
+        namespace={"configs": configs},
+        frozen=True,
+    )
+
+    return ConfigProduct
+
+
+ProblemConfigProduct = make_product_class(ProblemConfig)
+SketchingConfigProduct = make_product_class(SketchingConfig)
+PreconditioningConfigProduct = make_product_class(PreconditioningConfig)
+
+
+@dataclasses.dataclass(frozen=True)
+class ExperimentConfig(DaciteFromFile):
+    """ Stores all configuration options concerning all experiments """
+
+    number_of_runs: int = 1
+    problem_config_product: ProblemConfigProduct = ProblemConfigProduct()
+    sketching_config_product: SketchingConfigProduct = SketchingConfigProduct()
+    preconditioning_config_product: PreconditioningConfigProduct = (
+        PreconditioningConfigProduct()
+    )
+
+    def __post_init__(self):
+        """ Validates the current configuration """
+        try:
+            assert self.number_of_runs >= 1
+        except AssertionError as error:
+            raise ValueError("Invalid value in configuration") from error
+
+    def problem_configs(self) -> typing.Iterator[ProblemConfig]:
+        yield from self.problem_config_product.configs()
+
+    def sketching_configs(self) -> typing.Iterator[SketchingConfig]:
+        yield from self.sketching_config_product.configs()
+
+    def preconditioning_configs(self) -> typing.Iterator[PreconditioningConfig]:
+        yield from self.preconditioning_config_product.configs()
