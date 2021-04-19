19457dd4f889b371253b8b2bbc60adf30fd5e0e6
BurningKarl <10158964+BurningKarl@users.noreply.github.com>
2021-04-19 13:29:28 +0200

    Added graph of the sketching duration

--- graphs/plot_linear_operators.py
+++ graphs/plot_linear_operators.py
@@ -23,16 +23,59 @@ from plotting_utils import load_data, set_plot_aesthetics
 
 def graph_sketching_duration(ax, all_data, summary_data):
     # Bar chart
-    # x-axis: linear_operators=False, linear_operators=True and
-    #         linear_operators=True & triangular_solve=True
+    # x-axis: linear_operators_mode
     # y-axis: generate_sketch_duration + sketching_duration + decomposition_duration
-    pass
+
+    # Fill new "sketching_duration" column
+    filtered_data = all_data.copy()
+    filtered_data.loc[:, "sketching_duration"] = (
+        filtered_data.loc[:, "generate_sketch_duration"]
+        + filtered_data.loc[:, "sketching_duration"]
+        + filtered_data.loc[:, "decomposition_duration"]
+    )
+
+    sns.barplot(
+        data=filtered_data.reset_index(),
+        x="linear_operators_mode",
+        hue="linear_operators_mode",
+        dodge=False,
+        y="sketching_duration",
+        estimator=np.median,
+        ci="decile",
+        ax=ax,
+    )
+    ax.set(
+        xlabel="",
+        ylabel="Sketching duration [s]",
+        xticklabels=[r"Invert $\mathbf{R}$", "Triangular solve"],
+    )
+    ax.get_legend().remove()
 
 
 def main(args):
     set_plot_aesthetics()
 
     all_data, summary_data = load_data(args.group)
+
+    # Add "linear_operators_mode" column
+    all_data.loc[
+        all_data["linear_operators"] == False, "linear_operators_mode"
+    ] = "direct"
+    all_data.loc[
+        (all_data["linear_operators"] == True)
+        & (all_data["triangular_solve"] == False),
+        "linear_operators_mode",
+    ] = "linear_operator"
+    all_data.loc[
+        (all_data["linear_operators"] == True) & (all_data["triangular_solve"] == True),
+        "linear_operators_mode",
+    ] = "triangular_solve"
+
+    # Sort by linear_operators_mode
+    all_data.sort_values(by="linear_operators_mode", ascending=True, inplace=True)
+
+    # Filter out R^{-1} as linear operator
+    all_data = all_data.loc[all_data["linear_operators_mode"] != "linear_operator", :]
 
     fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
     graph_sketching_duration(axes[0], all_data, summary_data)
